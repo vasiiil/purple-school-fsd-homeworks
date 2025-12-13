71ffbfa785150943555cd34b3360d948c0e5fecb
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-12-13 22:44:33 +0300

    Установил vue-router и поправил конфиги

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths'; // 1. Импортируем
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [
+		vue(),
+		tsconfigPaths(), // 2. Подключаем
+	],
+	resolve: {
+		alias: {
+			// 3. Оставляем стандартный алиас для подстраховки
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
